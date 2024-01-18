079894b4fef2981fb334940274873ee75ab3b190
Erik Aakre <erikaakre@hotmail.com>
2024-01-17 22:27:07 -0600

    add cors to project

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const { handleAuthorization } = require("./middlewares/auth");
 const { login, createUser } = require("./controllers/users");
 const cors = require("cors");
 
-app.use(cors());
-
 const { PORT = 3001 } = process.env;
 const app = express();
+app.use(cors());
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/wtwr_db",
