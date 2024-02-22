1a6029675981d89674c1bc62cfdc70e9771ef754
Erik Aakre <erikaakre@hotmail.com>
2024-02-21 21:06:09 -0600

    update node env and jwt

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
-const JWT_SECRET =
-  "ee8f9cbd51e1f828e0d0e6ad704bd44a8d76d9b3e156e003914be2036b9453a2";
+const { JWT_SECRET = "dev-secret", NODE_ENV } = process.env;
 
 module.exports = {
   JWT_SECRET,
+  NODE_ENV,
 };
